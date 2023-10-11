327b9dc5739d033828cefeb39414b18f4442e446
Lukas Wöhrl <lukas.woehrl@plentymarkets.com>
2023-10-11 14:39:48 +0200

    feat: migrate to v3

--- operators.go
+++ operators.go
@@ -1,42 +1,86 @@
 package plugin
 
 import (
+	_ "embed"
 	"net"
 	"strconv"
 
-	"github.com/corazawaf/coraza/v2"
-	"github.com/corazawaf/coraza/v2/types/variables"
-	geoip "github.com/oschwald/geoip2-golang"
+	"github.com/corazawaf/coraza/v3/collection"
+	"github.com/corazawaf/coraza/v3/experimental/plugins/plugintypes"
+	"github.com/corazawaf/coraza/v3/types/variables"
+	"github.com/oschwald/geoip2-golang"
 )
 
-type geoLookup struct{}
 
-func (o *geoLookup) Init(data string) error {
-	return nil
+type geo struct {
+	db 		 *geoip2.Reader
+	dbtype   string
 }
 
-func (o *geoLookup) Evaluate(tx *coraza.Transaction, value string) bool {
-	db, ok := tx.Waf.Config.Get("geoip", nil).(*geoip.Reader)
-	if !ok || db == nil {
-		return false
+func newGeolookupCreator(db *geoip2.Reader, databaseType string) func(options plugintypes.OperatorOptions) (plugintypes.Operator, error) {
+	return func(options plugintypes.OperatorOptions) (plugintypes.Operator, error) {
+		return newGeolookup(options, db, databaseType)
 	}
+}
 
-	ip := net.ParseIP(value)
-	r, err := db.City(ip)
+func newGeolookup(options plugintypes.OperatorOptions, db *geoip2.Reader, databaseType string) (plugintypes.Operator, error) {
+	return &geo{db: db, dbtype: databaseType}, nil
+}
+
+func (o *geo) ApplyVariablesCity(col collection.Map, ip net.IP) bool {
+	r, err := o.db.City(ip)
 	if err != nil {
-		// do we generate an error?
 		return false
 	}
 
-	tx.GetCollection(variables.Geo).Set("country_code", []string{r.Country.IsoCode})
-	tx.GetCollection(variables.Geo).Set("country_name", []string{r.Country.Names["en"]})
-	tx.GetCollection(variables.Geo).Set("country_continent", []string{r.Continent.Names["en"]})
-	tx.GetCollection(variables.Geo).Set("region", []string{""})
-	tx.GetCollection(variables.Geo).Set("city", []string{r.City.Names["en"]})
-	tx.GetCollection(variables.Geo).Set("postal_code", []string{r.Postal.Code})
-	tx.GetCollection(variables.Geo).Set("latitude", []string{strconv.FormatFloat(r.Location.Latitude, 'f', 10, 64)})
-	tx.GetCollection(variables.Geo).Set("longitude", []string{strconv.FormatFloat(r.Location.Longitude, 'f', 10, 64)})
+	col.Set("country_code", []string{r.Country.IsoCode})
+	col.Set("country_name", []string{r.Country.Names["en"]})
+	col.Set("country_continent", []string{r.Continent.Names["en"]})
+	col.Set("region", []string{""})
+	col.Set("city", []string{r.City.Names["en"]})
+	col.Set("postal_code", []string{r.Postal.Code})
+	col.Set("latitude", []string{strconv.FormatFloat(r.Location.Latitude, 'f', 10, 64)})
+	col.Set("longitude", []string{strconv.FormatFloat(r.Location.Longitude, 'f', 10, 64)})
+
 	return true
 }
 
-var _ coraza.RuleOperator = &geoLookup{}
+func (o *geo) ApplyVariablesCountry(col collection.Map, ip net.IP) bool {
+	r, err := o.db.Country(ip)
+	if err != nil {
+		return false
+	}
+
+	col.Set("country_code", []string{r.Country.IsoCode})
+	col.Set("country_name", []string{r.Country.Names["en"]})
+	col.Set("country_continent", []string{r.Continent.Names["en"]})
+
+	return true
+}
+
+
+func (o *geo) Evaluate(tx plugintypes.TransactionState, value string) bool {
+	ip := net.ParseIP(value)
+
+	var col collection.Map
+	if c, ok := tx.Collection(variables.Geo).(collection.Map); !ok {
+		tx.DebugLogger().Error().Msg("collection in getset is not a map")
+		return false
+	} else {
+		col = c
+	}
+	if col == nil {
+		tx.DebugLogger().Error().Msg("collection in getset is nil")
+		return false
+	}
+
+	switch(o.dbtype) {
+	case "city":
+		return o.ApplyVariablesCity(col, ip)
+	case "country":
+		return o.ApplyVariablesCountry(col, ip)
+	default:
+		tx.DebugLogger().Error().Msg("invalid database type")
+		return false
+	}
+}
--- plugin.go
+++ plugin.go
@@ -1,12 +1,16 @@
 package plugin
 
 import (
-	"github.com/corazawaf/coraza/v2"
-	"github.com/corazawaf/coraza/v2/operators"
-	"github.com/corazawaf/coraza/v2/seclang"
+	"github.com/corazawaf/coraza/v3/experimental/plugins"
+	"github.com/oschwald/geoip2-golang"
 )
 
-func init() {
-	operators.RegisterPlugin("geoLookup", func() coraza.RuleOperator { return new(geoLookup) })
-	seclang.RegisterDirectivePlugin("secGeoLookupDb", directiveSecGeoLookupDb)
+func RegisterGeoDatabase(database []byte, databaseType string) {
+	db, err := geoip2.FromBytes(database)
+	if err != nil {
+		panic(err)
+	}
+
+	plugins.RegisterOperator("geoLookup", newGeolookupCreator(db, databaseType))
 }
+
